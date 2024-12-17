6b48b58b78a2a1ab776d28785da6119d90d8206f
Korwin <47688600+KorwinTeo@users.noreply.github.com>
2024-12-17 19:15:09 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 // ! 这里的数据在客户端、服务端都可调用，不要在这里存储敏感数据
 
 /** 网站名称 */
-export const WEBSITE_NAME = 'BMM'
+export const WEBSITE_NAME = 'NavTree'
 
 export const IS_DEV = process.env.NODE_ENV === 'development'
 
